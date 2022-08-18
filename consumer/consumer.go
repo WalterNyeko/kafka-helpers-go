b8829906c861b7a8a100e296cc4b5104505ab303
WalterNyeko <nyekowalter69@gmail.com>
2022-08-18 22:35:42 +0300

    implement consumer operations

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -3,58 +3,46 @@ package consumer
 import (
 	"context"
 	. "github.com/WalterNyeko/kafka-helpers-go/models"
-	. "github.com/WalterNyeko/kafka-helpers-go/utils"
 	"github.com/segmentio/kafka-go"
 	"log"
 	"os"
 	"strconv"
-	"time"
 )
 
 const (
 	failedToConnect = "Failed to dial leader:"
 	failedToCloseBatchReader = "Failed to close batch reader:"
 	failedToCloseWriter = "Failed to close writer:"
+	groupId = "G1"
 )
 
 func ConsumeMessage(topic string) ServiceMessage {
 
-	writeTimeout, _ := os.LookupEnv("KAFKA_WRITE_TIMEOUT")
 	address, _ := os.LookupEnv("KAFKA_ADDRESS")
-	network, _ := os.LookupEnv("KAFKA_NETWORK")
 	partition, _ := os.LookupEnv("KAFKA_PARTITION")
-
 	partitionValue, _ := strconv.Atoi(partition)
-	conn, err := kafka.DialLeader(context.Background(), network, address, topic, partitionValue)
-	if err != nil {
-		log.Println(failedToConnect, err)
-		return ErrorMessage(err, failedToConnect)
+
+
+	config := kafka.ReaderConfig{
+		Brokers: []string{address},
+		Topic: topic,
+		GroupID: groupId,
+		MinBytes: 10e3,
+		MaxBytes: 1e6,
+		Partition: partitionValue,
 	}
 
-	writeTimeoutValue, _ := strconv.Atoi(writeTimeout)
-	conn.SetReadDeadline(time.Now().Add(time.Duration(writeTimeoutValue)*time.Second))
-	batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
+	reader := kafka.NewReader(config)
 
-	b := make([]byte, 10e3) // 10KB max per message
 	for {
-		n, err := batch.Read(b)
+		m, err := reader.ReadMessage(context.Background())
 		if err != nil {
-			break
+			continue
+			return ServiceMessage{}
 		}
-		log.Println(string(b[:n]))
+		log.Println(string(m.Value))
 		return ServiceMessage{
-			ResponseData: string(b[:n]),
+			ResponseData: string(m.Value),
 		}
 	}
-
-	if err := batch.Close(); err != nil {
-		log.Println(failedToCloseBatchReader, err)
-		return ErrorMessage(err, failedToCloseBatchReader)
-	}
-
-	if err := conn.Close(); err != nil {
-		log.Println(failedToCloseWriter, err)
-		return ErrorMessage(err, failedToCloseWriter)
-	}
-	return ServiceMessage{}
 }
